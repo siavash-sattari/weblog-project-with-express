afedb57939dab194567a3d6b006e2a4fc3a168ec
siavash sattari <siavash.sattari.75@gmail.com>
2022-07-15 01:57:21 -0700

    test - pass date from controller to dashboard

--- app/controllers/admin/dashboard.js
+++ app/controllers/admin/dashboard.js
@@ -1,3 +1,10 @@
 exports.index = (req, res) => {
-  res.render('admin/dashboard/index', { layout: 'admin' });
+  const data = {
+    totlaUsers: 10,
+    totalPosts: 20,
+    totalComments: 30,
+    totalViews: 40
+  };
+
+  res.render('admin/dashboard/index', { layout: 'admin', ...data });
 };
