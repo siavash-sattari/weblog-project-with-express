57ae1518e602a332089006d390f4beea90961f55
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-01 04:36:46 -0700

    display the author's name

--- app/models/post.js
+++ app/models/post.js
@@ -1,6 +1,10 @@
 const db = require('@database/mysql');
 
 exports.findAll = async () => {
-  const [rows] = await db.query('SELECT * FROM posts');
+  const [rows] = await db.query(`
+  SELECT p.*,u.full_name 
+  FROM posts p 
+  JOIN users u ON p.author_id = u.id
+  `);
   return rows;
 };
