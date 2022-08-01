60f35fa5d38bffdc57657f6819f4edff1421917b
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-01 04:06:30 -0700

    test

--- app/controllers/admin/posts.js
+++ app/controllers/admin/posts.js
@@ -2,6 +2,5 @@ const postModel = require('@models/post');
 
 exports.index = async (req, res) => {
   const posts = await postModel.findAll();
-  console.log(posts);
   res.render('admin/posts/index', { layout: 'admin', posts });
 };
